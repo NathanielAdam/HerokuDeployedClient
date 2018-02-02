f0ecaf5b8f2431dff83017eeb53288e0c85728ed
Nathaniel Adam <grizmoore95@gmail.com>
2018-02-02 11:12:58 -0500

    Howdy buttons

--- workouts/groups.js
+++ workouts/groups.js
@@ -1,10 +1,23 @@
 $(function() {
     $.extend(WorkoutLog, {
         Comrades: {
-            comrade [],
+            comrade: [],
 
             PopulateUsers: 
             function() {
+               
+                    var defs = WorkoutLog.definition.userDefinitions;
+                    var len = defs.length;
+                    console.log(len)
+                    var opts;
+                    for (var i = 0; i <len; i++) {
+                        opts += "<option value='" + defs[i].id +"'>" + defs[i].description +"</option>";
+                    }
+                    $('#log-definition').children().remove();
+                    $('#log-definition').append(opts);
+                    $('#update-definition').children().remove();
+                    $('#update-definition').append(opts);
+                
                 
             }
         }
